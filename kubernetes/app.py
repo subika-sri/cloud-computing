3c5a7524061dcfe0a51ac9eb3c37aa938ed282bf
subika-sri <90998998+subika-sri@users.noreply.github.com>
2022-02-14 15:18:51 +0530

    app.py

--- kubernetes/app.py
+++ kubernetes/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
+import time
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify({"Time of Call" : time.time()})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
